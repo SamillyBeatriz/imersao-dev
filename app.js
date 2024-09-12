4d430beac28c958878b721720b8db8ce6dde79d4
Samilly Martins <samilly.beatriz23@gmail.com>
2024-09-12 01:28:29 -0300

    implementa funcionalidade nos botões que mostra a estrutura dos termos e melhora lógica de pesquisa do termo

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
 function pesquisarTermo() {
-    // Seleciona a seção onde os resultados da pesquisa serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
+  //seleciona a seção onde os resultados da pesquisa serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  //seleciona o valor digitado no campo de pesquisa
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  //seleciona os botões das linguagens
+  const botoesEstrutura = document.querySelectorAll('.botao-mostrarEstrutura');
   
-    // Inicializa uma string vazia para armazenar os resultados da pesquisa
-    let resultados = "";
-  
-    // Itera sobre cada dado na lista de dados
-    for (let dado of dados) {
+  //transforma entrada de texto em texto com letras minusculas
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  //impede o click no botão pesquisar sem nenhum termo
+  if (campoPesquisa === "") {
+    return
+  }
+  // Inicializa uma string vazia para armazenar os resultados da pesquisa
+  let resultadosTermos = "";
+
+  //variaveis para manusear titulo e descrição de dados.js
+  let titulo = "";
+  let descricao = "";
+
+  // Itera sobre cada dado na lista de dados
+  for (let dado of dados) {
+
+    //transforma o titulo de dado em letras minusculas
+    titulo = dado.titulo.toLocaleLowerCase();
+    descricao = dado.descricao.toLocaleLowerCase();
+    //verifica se o termo digitado está na base de dados
+    if (dado.titulo.includes(campoPesquisa)) {
       // Cria uma nova div para cada resultado
-      resultados += `
+      resultadosTermos += `
         <div class="item-resultado">
           <h2>${dado.titulo}</h2>
           <p class="descricao-meta">${dado.descricao}</p>
-          <button onclick="" class="botao-mostrarEstrutura">JavaScript</button>
-          <button onclick="" class="botao-mostrarEstrutura">Java</button>
-          <button onclick="" class="botao-mostrarEstrutura">Python</button>
+          <button  class="botao-mostrarEstrutura" id="JavaScript">JavaScript</button>
+          
+          <button class="botao-mostrarEstrutura" id="Java">Java</button>
+          
+          <button class="botao-mostrarEstrutura" id="Python">Python</button>
+          <div id="exibicaoEstrutura"></div>
           <p>Mais Informações:</p>
           <ul>
             <li><a href=${dado.links[0]} target="_blank">Site 1</a></li>
@@ -22,7 +48,53 @@ function pesquisarTermo() {
           </ul>
         </div>`;
     }
-  
-    // Atualiza o conteúdo da seção com os resultados da pesquisa
-    section.innerHTML = resultados;
   }
+
+  //se não encontrar o termo na base de dados
+  if (!resultadosTermos) {
+    resultadosTermos = "Nada Encontrado. Digite um termo!"
+  }
+
+  // Atualiza o conteúdo da seção com os resultados da pesquisa
+  section.innerHTML = resultadosTermos;
+
+  //adiciona o evento de mostrar estrutura nos botões das linguagens
+  botoesEstrutura.forEach(botao => {
+    botao.addEventListener('click', function () {
+      const linguagem = this.id;//pega a linguagem do botao
+      // Encontrar o objeto do termo pesquisado
+      const termoPesquisado = dados.find(dado => dado.titulo === campoPesquisa);
+
+      if (termoPesquisado) {//se encontrou o termo
+        mostrarEstrutura(linguagem, termoPesquisado);//mostra a estrutura atraves da função
+      } else {
+        console.error("Termo não encontrado.");
+      }
+
+    });
+  });
+}
+
+function mostrarEstrutura(linguagem, termoPesquisado) {
+
+  // obtem o id do paragrafo onde sera exibido a estrutura
+  const paragrafoEstrutura = document.getElementById('exibicaoEstrutura');
+
+  // Cria um objeto para mapear linguagens para índices numéricos
+  const indicesLinguagens = {
+    'JavaScript': 0,
+    'Java': 1,
+    'Python': 2
+  };
+
+  // Obtém o índice da linguagem correspondente no objeto de índices
+  const indiceLinguagem = indicesLinguagens[linguagem];
+
+  // Acessa a estrutura específica da linguagem dentro do objeto termoPesquisado
+  const estruturaEncontrada = termoPesquisado.estrutura[indiceLinguagem];
+
+  // Atualiza o conteúdo do parágrafo com a estrutura encontrada
+  paragrafoEstrutura.textContent = `${estruturaEncontrada}`;
+}
+
+
